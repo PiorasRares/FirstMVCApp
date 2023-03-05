56ead3778589aeeb14d176d1f5dc01e094727609
PiorasRares <pioras.rares95@gmail.com>
2023-03-05 17:12:12 +0200

    Add project files.

---
+++ FirstMVCApp/Models/CodeSnippetModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstMVCApp.Models
+{
+    public class CodeSnippetModel
+    {
+        [Key]
+        public Guid IdCodeSnippet { get; set; }
+
+        [StringLength(100, ErrorMessage ="Titlul poate contine max 100 caractere!")]
+        public string Title { get; set; }
+
+        public string ContentCode { get; set; }
+
+        public Guid IdMember { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage="Revision number trebuie sa fie pozitiv!")]
+        public int Revision { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime DateTimeAdded { get; set; }
+    }
+}
